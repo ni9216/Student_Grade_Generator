4b5abbea2a8160053aeacb8ef12c21f666266cff
liech <nicholasliech851@gmail.com>
2024-06-30 19:44:24 +0300

    commit

--- solution2.js
+++ solution2.js
@@ -1,20 +1,23 @@
-function checkSpeed(speed){
-    //limit in km/h
-const speedLimit = 70;
-let demeritPoints = 0;
-
-//if the speed is below the limit
-if (speed <= speedLimit){
-    console.log("Ok")
-} else{
+function speedDetector(speed) {
+    const speedLimit = 70;
+  let demeritPoints = 0 ;
+ 
+  
+  //condition check if the speed is low limit
+  if (speed < speedLimit) {
+     console.log("Ok");
+  } else{
     const excessSpeed = speed - speedLimit;
-demeritPoints = Math.floor(excessSpeed / 5);
+    demeritPoints = Math.floor(excessSpeed / 5);
+    if(demeritPoints > 12){
+        console.log(" License suspended");
+  } else{
+    console.log( `points: ${demeritPoints}`);
+  }
+   
+  }
+}
 
-if (demeritPoints> 12){
-    console.log("license suspended");
-} else{
-    console.log(`Points: ${demeritPoints}`);
-}
-}
-}
-
+//speedDetector(80);
+speedDetector(130);
+ 
